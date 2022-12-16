f7a14d2b809738363dcfacdcfe8bde233e3d79c4
Jhonleandres <jhonleandres.silva@gmail.com>
2022-12-16 11:05:38 -0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './src/pages/**/*.{html,js,ts,jsx,tsx}',
     './src/components/**/*.{html,js,ts,jsx,tsx}',
+    './src/templates/**/*.{html,js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
